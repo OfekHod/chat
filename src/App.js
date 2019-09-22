6e420a3ffc60390229609f8c9ef2754cc52ec5d1
unknown <1xofekx1@gmail.com>
2019-09-22 15:19:11 +0300

    first working version with complete messages components and css classes

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ import './App.css';
 class Message extends React.Component {
  render() {
    return (
-     <p className="message">
-       {this.props.message}
-     </p>
+     <div className="message">
+       <div className="message-details">
+          <div className="message-sender">
+            {this.props.sender}
+          </div>
+          <div className="message-date">
+            {this.props.date}
+          </div>
+        </div>
+        <div className="message-content">
+          {this.props.content}
+        </div>
+     </div>
    );
  }
 }
 
+class Bar extends React.Component {
+  render() {
+    return(
+      <div className="bar">
+          {this.props.content}
+      </div>
+      )
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Chat.
-        </p>
-        <Message message="Ofek gever"/>
-      </header>
+    <div className="App"> 
+      <Bar content="Chat"/>
+      <div className="message-window">
+        <Message sender="Ofek" date="22/09/2019 14:43" content="Hey, how are you?"/>
+        <Message sender="Shahar" date="22/09/2019 14:43" content="I'm fine, thank you Ofek!"/>
+        <Message sender="Ofek" date="22/09/2019 14:43" content="Well, how is this chat?"/>
+        <Message sender="Shahar" date="22/09/2019 14:43" content="Man this is great!! we should talk here all the time"/>
+        <Message sender="Ofek" date="22/09/2019 14:43" content=";)"/>
+      </div>
+      <Bar content=""/>        
     </div>
   );
 }
--- src/chat.js
+++
@@ -1,9 +0,0 @@
-class Message extends React.Component {
-	render() {
-		return(
-			<button className="message">
-			{this.props.message}
-			</button>
-		)
-	}
-}
